004ffb47f6167ff77713dcdfdf9f4fe181125ec2
RukmoddinPatel <rukmoddinpatel27@gmail.com>
2024-03-02 22:32:03 +0530

    First Ever Commit

---
+++ Launch1.java.java
@@ -0,0 +1,8 @@
+public class launch{
+	public static void main(String[] args){
+	System.out.println("RUkmoddin Nabab Patel");
+	System.out.println("I want to be a good human being");
+	}
+}
+
+//R.N.P
